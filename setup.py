c3f702fedbe5fe192d4e4a7a3cfa81df163cd282
Minseok Oh <minsec9912@icloud.com>
2025-06-16 12:28:12 +0900

    setup.py installs requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import find_packages, setup
 
+
+def parse_requirements(filename: str) -> list[str]:
+    with open(file=filename, mode="r") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+
 setup(
     name="aspa",
     version="0.1.0",
     author="Daniel Oh",
     description="Audio Signal Processing & Analysis (ASPA) is a Python package for audio signal processing and analysis.",
-    packages=find_packages(include=[".", ".*"]),
+    packages=find_packages(include=["aspa", "aspa.*"]),
+    install_requires=parse_requirements("requirements.txt"),
 )
