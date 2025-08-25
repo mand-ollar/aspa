ecb97b896f2aed4b2c4ac89d44aa5c0bc9a176d7
Minseok Oh <minsec9912@icloud.com>
2025-08-25 16:13:19 +0900

    add to __init__

--- aspa/audio/windowing/__init__.py
+++ aspa/audio/windowing/__init__.py
@@ -1,6 +1,6 @@
 from .config import WindowingConfig
 from .dataset import BaseWindowingDataset, IndexedWindowingDataset, WindowingDataset
-from .filters import ModelInferenceFilter, RMSFilter
+from .filters import BaseFilter, ModelInferenceFilter, RMSFilter
 from .types import WindowingResult
 from .windowing import Windowing
 
@@ -9,6 +9,7 @@ __all__ = [
     "BaseWindowingDataset",
     "IndexedWindowingDataset",
     "WindowingDataset",
+    "BaseFilter",
     "ModelInferenceFilter",
     "RMSFilter",
     "WindowingResult",
