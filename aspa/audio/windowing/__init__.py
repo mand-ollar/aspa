6f09cd9c43a1b9a2581e35809beebdf114654f12
Minseok Oh <minsec9912@icloud.com>
2025-09-01 12:01:06 +0900

    remove filter

--- aspa/audio/windowing/__init__.py
+++ aspa/audio/windowing/__init__.py
@@ -1,6 +1,5 @@
 from .config import WindowingConfig
 from .dataset import BaseWindowingDataset, IndexedWindowingDataset, WindowingDataset
-from .filters import BaseFilter, ModelInferenceFilter, RMSFilter
 from .types import WindowingResult
 from .windowing import Windowing
 
@@ -9,9 +8,6 @@ __all__ = [
     "BaseWindowingDataset",
     "IndexedWindowingDataset",
     "WindowingDataset",
-    "BaseFilter",
-    "ModelInferenceFilter",
-    "RMSFilter",
     "WindowingResult",
     "Windowing",
 ]
